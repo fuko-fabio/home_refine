aef5b65ddf5bd99b1c42d1d102b9e0130fd15acf
Norbert Pabian <norbert.pabian@xing.com>
2016-12-22 09:30:32 +0100

    Header styles

--- wp-content/themes/illdy/header.php
+++ wp-content/themes/illdy/header.php
@@ -15,31 +15,6 @@ $jumbotron_single_image    = get_theme_mod( 'illdy_jumbotron_enable_featured_ima
 $jumbotron_parallax_enable = get_theme_mod( 'illdy_jumbotron_enable_parallax_effect', true );
 $preloader_enable          = get_theme_mod( 'illdy_preloader_enable', 1 );
 
-$style = '';
-
-if ( get_option( 'show_on_front' ) == 'page' && is_front_page() ) {
-	if ( $jumbotron_general_image ) {
-		$style = 'background-image: url(' . esc_url( $jumbotron_general_image ) . ');';
-	}
-} else if ( ( is_single() || is_page() ) && $jumbotron_single_image == true ) {
-
-	global $post;
-	if ( has_post_thumbnail( $post->ID ) ) {
-		$style = 'background-image: url(' . esc_url( get_the_post_thumbnail_url( $post->ID, 'full' ) ) . ');';
-	}
-} else {
-	$style = 'background-image: url(' . get_header_image() . ');';
-}
-
-// append the parallax effect
-if ( $jumbotron_parallax_enable == true ) {
-	$style .= 'background-attachment: fixed;';
-}
-
-if ( ( is_single() || is_page() || is_archive() ) && get_theme_mod( 'illdy_archive_page_background_stretch' ) == 2 ) {
-	$style .= 'background-size:contain;background-repeat:no-repeat;';
-}
-
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -53,14 +28,14 @@ if ( ( is_single() || is_page() || is_archive() ) && get_theme_mod( 'illdy_archi
 	<div class="pace-overlay"></div>
 <?php endif; ?>
 <header id="header" class="<?php if ( get_option( 'show_on_front' ) == 'page' && is_front_page() ): echo 'header-front-page';
-else: echo 'header-blog'; endif; ?>" style="<?php echo $style ?>">
+else: echo 'header-blog'; endif; ?>" >
 	<div class="top-header">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-2 col-xs-8">
 
 					<?php if ( ! empty( $logo_image ) ) { ?>
-						<?php echo '<a href="' . esc_url( home_url() ) . '"><img src="' . esc_url( $logo_image[0] ) . '" /></a>'; ?>
+						<?php echo '<a href="' . esc_url( home_url() ) . '"><img class="header-logo-img" src="' . esc_url( $logo_image[0] ) . '" /></a>'; ?>
 					<?php } else { ?>
 						<?php if ( get_option( 'show_on_front' ) == 'page' ) { ?>
 							<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( $text_logo ); ?>" class="header-logo"><?php echo esc_html( $text_logo ); ?></a>
--- wp-content/themes/illdy/sections/front-page-bottom-header.php
+++ wp-content/themes/illdy/sections/front-page-bottom-header.php
@@ -27,38 +27,69 @@ if ( current_user_can( 'edit_theme_options' ) ) {
 	$second_button_url     = get_theme_mod( 'illdy_jumbotron_general_second_button_url' );
 }
 
+$jumbotron_general_image   = get_theme_mod( 'illdy_jumbotron_general_image', esc_url( get_template_directory_uri() . '/layout/images/front-page/front-page-header.jpg' ) );
+
+$style = '';
+
+if ( get_option( 'show_on_front' ) == 'page' && is_front_page() ) {
+	if ( $jumbotron_general_image ) {
+		// linear-gradient(90deg, rgba(0,0,0,0) 0, rgba(0,0,0,0.8) 100%), 
+		$style = 'background-image: url(' . esc_url( $jumbotron_general_image ) . ');';
+	}
+} else if ( ( is_single() || is_page() ) && $jumbotron_single_image == true ) {
+
+	global $post;
+	if ( has_post_thumbnail( $post->ID ) ) {
+		$style = 'background-image: url(' . esc_url( get_the_post_thumbnail_url( $post->ID, 'full' ) ) . ');';
+	}
+} else {
+	$style = 'background-image: url(' . get_header_image() . ');';
+}
+
+// append the parallax effect
+if ( $jumbotron_parallax_enable == true ) {
+	$style .= 'background-attachment: fixed;';
+}
+
+if ( ( is_single() || is_page() || is_archive() ) && get_theme_mod( 'illdy_archive_page_background_stretch' ) == 2 ) {
+	$style .= 'background-size:contain;background-repeat:no-repeat;';
+}
+
 if ( $first_row_from_title || $second_row_from_title || $third_row_from_title || $entry || $first_button_title || $second_button_title ) {
 
 	?>
-	<div class="bottom-header front-page">
+	<div class="bottom-header front-page" style="<?php echo $style ?>">
 		<div class="container">
 			<div class="row">
 				<?php if ( $first_row_from_title || $second_row_from_title || $third_row_from_title ): ?>
-					<div class="col-sm-12">
+					<div class="col-sm-10">
 						<h2><?php if ( $first_row_from_title ) {
-								echo '<span data-customizer="first-row-from-title">' . illdy_sanitize_html( $first_row_from_title ) . '</span><span class="span-dot first-span-dot">' . __( '.', 'illdy' ) . '</span>';
+								echo '<span data-customizer="first-row-from-title">' . illdy_sanitize_html( $first_row_from_title ) . '</span>';
 							} ?>
 							<?php if ( $second_row_from_title ) {
-								echo '<span data-customizer="second-row-from-title">' . illdy_sanitize_html( $second_row_from_title ) . '</span><span class="span-dot second-span-dot">' . __( '.', 'illdy' ) . '</span>';
+								echo '<span data-customizer="second-row-from-title">' . illdy_sanitize_html( $second_row_from_title ) . '</span>';
 							} ?>
 
 							<?php if ( $third_row_from_title ) {
 								echo '<span data-customizer="third-row-from-title">' . illdy_sanitize_html( $third_row_from_title ) . '</span>';
 							} ?>
 						</h2>
-					</div><!--/.col-sm-12-->
+						<?php if ( $entry ): ?>
+							<p><?php echo illdy_sanitize_html( $entry ); ?></p>
+						<?php endif; ?>
+					</div><!--/.col-sm-10-->
+					<div class="col-sm-2">
+						<?php echo '<a href="' . esc_url( home_url() ) . '"><img class="bottom-header-logo-img" src="' . esc_url_raw( get_template_directory_uri() . '/layout/images/logo-small.png' ) . '" /></a>'; ?>
+					</div><!--/.col-sm-2-->
 				<?php endif; ?>
-				<div class="col-sm-8 col-sm-offset-2">
-					<?php if ( $entry ): ?>
-						<p><?php echo illdy_sanitize_html( $entry ); ?></p>
-					<?php endif; ?>
+				<div class="col-sm-10">
 					<?php if ( $first_button_title ): ?>
 						<a href="<?php echo esc_url( $first_button_url ); ?>" title="<?php echo esc_attr( $first_button_title ); ?>" class="header-button-one"><?php echo esc_html( $first_button_title ); ?></a>
 					<?php endif; ?>
 					<?php if ( $second_button_title ): ?>
 						<a href="<?php echo esc_url( $second_button_url ); ?>" title="<?php echo esc_attr( $second_button_title ); ?>" class="header-button-two"><?php echo esc_html( $second_button_title ); ?></a>
 					<?php endif; ?>
-				</div><!--/.col-sm-8.col-sm-offset-2-->
+				</div><!--/.col-sm-10-->
 			</div><!--/.row-->
 		</div><!--/.container-->
 	</div><!--/.bottom-header.front-page-->
