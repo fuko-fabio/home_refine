603ccb04d8b8f0d412efef0aa059b45bdc619a0a
Norbert Pabian <norbert.pabian@xing.com>
2017-01-06 18:09:08 +0100

    Contact map

--- wp-content/themes/illdy/sections/front-page-contact-us.php
+++ wp-content/themes/illdy/sections/front-page-contact-us.php
@@ -98,12 +98,15 @@ if ( $general_title != '' || $general_entry != '' || $general_address_title != '
 					</div><!--/.col-sm-12-->
 				</div><!--/.row-->
 				<div class="row">
-					<div class="col-sm-12">
+					<div class="col-sm-4">
+						<iframe class="contact-us-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39816.54018902203!2d-0.24340738653834806!3d51.41162097951945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487608bef8bb6677%3A0x4b6cb3bffb792a2e!2s269+Kingston+Rd%2C+London+SW19+3NW%2C+Wielka+Brytania!5e0!3m2!1spl!2spl!4v1483711320845" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+					</div>
+					<div class="col-sm-8">
 						<?php if ( class_exists( 'WPCF7' ) && $general_contact_form_7 != null && $general_contact_form_7 != 'default' ): ?>
 							<?php $shortcode = '[contact-form-7 id="' . esc_html( $general_contact_form_7 ) . '"]'; ?>
 							<?php echo do_shortcode( $shortcode ); ?>
 						<?php endif; ?>
-					</div><!--/.col-sm-12-->
+					</div><!--/.col-sm-8-->
 				</div><!--/.row-->
 			</div><!--/.container-->
 		</div><!--/.section-content-->
